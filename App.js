93920cd25b8ed17af670d389ad96fe1a25fe7ef9
NitroGitRepo <rajneesh.yadav.aws@gmail.com>
2023-11-22 16:41:34 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,20 @@
+
+
+const heading  = React.createElement("h1", {
+      id:"Heading",
+      class:"Head",
+      
+}, " Welcome to React !!");
+// console.log(heading);
+
+const parent = React.createElement("div", {id:"parent"},
+         React.createElement("div",{id:"child"},
+          [React.createElement("h1", {id:"h1 tag"}, "I'am H1 Tag"),
+          React.createElement("h2", {id:"h1 tag"}, "I'am H1 Tag")] 
+          ) 
+)
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
